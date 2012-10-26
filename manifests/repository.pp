00ca226eafbf2ecfe00aa2f9fd1a62ad4536f124
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-10-26 10:46:06 +0200

    corrected bazaar::repository

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -7,30 +7,50 @@ define bazaar::repository(
 ) {
 
   file {"/srv/bzr/${name}":
-    ensure => $ensure ? {
-      present => directory,
-      default => absent
-    },
-    owner  => $owner,
-    group  => $group,
-    mode   => $mode,
+    owner => $owner,
+    group => $group,
+    mode  => $mode,
   }
 
-  exec {"init-repo ${name}":
-    command => $rootpack? {
-      true => "umask 0002; bzr init-repo --rich-root-pack /srv/bzr/${name}",
-      default => "umask 0002; bzr init-repo /srv/bzr/${name}",
-      },
-    user    => $owner,
-    creates => "/srv/bzr/${name}/.bzr",
-    require => Package["bzr"],
+  case $ensure {
+    present: {
+      File["/srv/bzr/${name}"] {
+        present => directory,
+      }
+
+      $init_command = $rootpack? {
+        true    => "umask 0002; bzr init-repo --rich-root-pack /srv/bzr/${name}",
+        default => "umask 0002; bzr init-repo /srv/bzr/${name}",
+      }
+
+      exec {"init-repo ${name}":
+        command => $init_command,
+        user    => $owner,
+        creates => "/srv/bzr/${name}/.bzr",
+        require => Package['bzr'],
+      }
+
+      file {"/srv/bzr/${name}/.bzr":
+        ensure  => $ensure,
+        owner   => $owner,
+        group   => $group,
+        mode    => $mode,
+        require => Exec["init-repo ${name}"],
+      }
+    }
+
+    absent: {
+      File["/srv/bzr/${name}"] {
+        ensure  => absent,
+        backup  => false,
+        force   => true,
+        recurse => true,
+      }
+    }
+
+    default: {
+      fail "Unknown \$ensure '${ensure}' for ${name} bazaar::repostitory"
+    }
   }
 
-  file {"/srv/bzr/${name}/.bzr":
-    ensure  => $ensure,
-    owner   => $owner,
-    group   => $group,
-    mode    => $mode,
-    require => Exec["init-repo ${name}"],
-  }
 }
