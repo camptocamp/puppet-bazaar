c54c0b59dda0dfdfc8f20edef54061808657c411
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-04-03 15:58:51 +0200

    Lint

--- manifests/rootrepository.pp
+++ manifests/rootrepository.pp
@@ -6,24 +6,28 @@ define bazaar::rootrepository(
   $rootpack = false
 ) {
 
+  $_ensure = $ensure ? {
+    'present' => directory,
+    default => absent
+  }
+
   file {"/srv/bzr/${name}":
-    ensure => $ensure ? {
-      present => directory,
-      default => absent
-    },
+    ensure => $_ensure,
     owner  => $owner,
     group  => $group,
     mode   => $mode,
   }
 
+  $command = $rootpack ? {
+    true    => "umask 0002; bzr init --rich-root-pack /srv/bzr/${name}",
+    default => "umask 0002; bzr init /srv/bzr/${name}",
+  }
+
   exec {"init ${name}":
-    command   => $rootpack? {
-      true    => "umask 0002; bzr init --rich-root-pack /srv/bzr/${name}",
-      default => "umask 0002; bzr init /srv/bzr/${name}",
-      },
-    user      => $owner,
-    creates   => "/srv/bzr/${name}/.bzr",
-    require   => Package['bzr'],
+    command => $command,
+    user    => $owner,
+    creates => "/srv/bzr/${name}/.bzr",
+    require => Package['bzr'],
   }
 
   file {"/srv/bzr/${name}/.bzr":
