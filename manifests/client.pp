215aa9d32d315dc11634119c8f309e2882ee7c29
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-12-02 09:21:48 +0100

    Don't install bzrtools on lenny

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,8 +1,14 @@
 class bazaar::client {
   case $::osfamily {
     'Debian': {
-      package{['bzr', 'bzrtools', 'python-paramiko']:
-        ensure => installed,
+      if $::operatingsystem == 'Debian' and versioncmp($::operatingsystemmajrelease, '6') < 0 {
+        package {['bzr', 'python-paramiko']:
+          ensure => present,
+        }
+      } else {
+        package{['bzr', 'bzrtools', 'python-paramiko']:
+          ensure => installed,
+        }
       }
     }
     default: {
