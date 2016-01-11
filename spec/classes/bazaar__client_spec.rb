73ed68fbfeb046eaeb91e0520566eff07ed9a251
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-01-11 15:15:56 +0100

    Add unit tests

---
+++ spec/classes/bazaar__client_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'bazaar::client' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
